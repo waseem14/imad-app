42d96771be0132af64e915add49e071bf531ad6f
waseem14 <30650726+waseem14@users.noreply.github.com>
2017-09-17 11:24:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res) {
+app.get('/article-one',function (req,res) {
     res.sendFile(path.join(_dirname,'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res) {
