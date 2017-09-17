2936531cc5cf6f3f2e64ade7e056c672365f6306
waseem14 <30650726+waseem14@users.noreply.github.com>
2017-09-17 16:43:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function (req,res) {
-    var articleName=get.params.articleName;
+    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
