f5d9229d821ef565a09bb33e879bb5f87328d799
waseem14 <30650726+waseem14@users.noreply.github.com>
2017-09-17 10:57:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res) {
+    res.send("Article one is requested and stored here");
+});
+app.get('/article-two',function(req,res) {
+    res.send("Article two  is requested and stored here");
+});
+
+app.get('/article-three',function(req,res) {
+    res.send("Article three is requested and stored here");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
