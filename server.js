ccccd97510800244d2ad076c31b85c500363d98c
waseem14 <30650726+waseem14@users.noreply.github.com>
2017-09-17 11:26:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req,res) {
-    res.sendFile(path.join(_dirname,'ui', 'article-one.html'));
+    res.sendFile(path.join(_dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res) {
     res.send("Article two  is requested and stored here");
