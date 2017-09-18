a60ed6a25122f0882de84b9e0ca4b8c0cfb0aeee
waseem14 <30650726+waseem14@users.noreply.github.com>
2017-09-18 21:08:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML="new value";
 //move image
 var img=document.getElementById('image');
 
-element.onClick=function(){
+element.onClick = function(){
     element.style.marginLeft= "500px";
     
     
