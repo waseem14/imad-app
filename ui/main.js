6a3406463e274645fc7c9eb6ebbf4ff60094a44f
waseem14 <30650726+waseem14@users.noreply.github.com>
2017-09-18 20:55:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,14 +3,11 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="new value";
 //move image
-var image=document.getElementById('img');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft +5;
-    img.style.marginLeft=marginLeft +'px';
-}
+var img=document.getElementById('img');
 
-image.onClick=function(){
-    var interval=setInterval(moveRight,50);
-
+img.onClick=function(){
+    img.style.marginLeft='200px';
+    
+    
+    
 };
