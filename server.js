4c40962293bd7ff9f859b81bdaa99911c85abbcb
waseem14 <30650726+waseem14@users.noreply.github.com>
2017-09-23 20:20:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/counter',function(req, res){
-    counter=counter+1;
-    res.send(counter.toString());
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
