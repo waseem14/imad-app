7a9d48773378b9b586fc8abb04365d667dda1f5e
waseem14 <30650726+waseem14@users.noreply.github.com>
2017-09-23 19:50:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,21 @@
 //move image
 //var img=document.getElementById('image');
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
+    //create request
     var request=new XMLHttpRequest();
-    counter=counter+1;
-    
- var span=document.getElementById('count');
- span.innerHTML=counter.toString();
-};
+    request.open('http://waseem1997.imad.hasura-app.io/counter',true);
+    request.send(null);
+    //capture the response
+    request.onreadystateChange=function(){
+        if(request.readyState==XMLHttpRequet.DONE){
+            //take action
+            if(request.status==200){
+                counter=counter+1;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+};    
